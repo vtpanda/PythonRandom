5a767d9697bf3434e70c8dedd7dea6c9887730e3
Matt Roberts <vtpanda@users.noreply.github.com>
2019-10-29 16:10:49 -0400

    Stuff

--- KinesisLab.py
+++ KinesisLab.py
@@ -8,33 +8,16 @@ import random
 
 
 client = boto3.client('firehose')
-url = "https://randomuser.me/api/?format=json"
+max = 10000
+i = 0
 
-response = requests.get(url)
-
-
-# statusCode = response.status_code
-
-new_person = json.dumps(response.json()).encode('utf-8')
-# testjson = response.json()
-#
-# testjson['results'][0]['name']['first']
-# testjson['results'][0]['name']['last']
-# testjson['results'][0]['dob']['age']
-# testjson['results'][0]['gender']
-# testjson['results'][0]['location']['coordinates']['latitude']
-# testjson['results'][0]['location']['coordinates']['longitude']
-
-
-# print(response.content)
-client.put_record(DeliveryStreamName='PersonLoad',
-    Record={
-        'Data': new_person
-    })
-
-# count = 0
-#
-# while (count < 10000):
-#     count = count + 1
-#
-#     time.sleep(random.uniform(0, 1))
+while i < max:
+    i += 1
+    response = requests.get("https://randomuser.me/api/?format=json")
+    # statusCode = response.status_code
+    new_person = json.dumps(response.json()).encode('utf-8')
+    client.put_record(DeliveryStreamName='PersonLoad',
+        Record={
+            'Data': new_person
+        })
+    time.sleep(random.uniform(0, 1))
